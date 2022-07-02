01b3f9a0badd8330d2dbff5890bebb00666fbc76
Rob Benney <rob.benney@gmail.com>
2022-07-02 11:55:06 -0400

    about me section setup

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import coverImage from '../../assets/images/coverImage/coverImage.jpg';
 
 function About() {
   return (
     <section>
-      <h1 id="about">About Me</h1>
+      <h1 id="about">Get To Know Me</h1>
+      <img src={coverImage} className='my-2' style={{ width: '30%' }} alt='cover' />
     </section>
   );
 }
